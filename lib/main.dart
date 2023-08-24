31ad30db192e454d57e7cb51d6e1463227f24307
3arfa11 <ngmmedo80@gmail.com>
2023-08-24 15:55:46 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,4 @@
-import 'package:ecommerceapp/Auth/signup_page.dart';
-import 'package:ecommerceapp/screens/fetures_page.dart';
-import 'package:ecommerceapp/screens/filter_page.dart';
-import 'package:ecommerceapp/screens/home_page.dart';
-import 'package:ecommerceapp/screens/item_details.dart';
-import 'package:ecommerceapp/screens/over_view_page.dart';
-import 'package:ecommerceapp/screens/search_page.dart';
-import 'package:ecommerceapp/screens/shopping_cart.dart';
-import 'package:ecommerceapp/screens/specific_category_page.dart';
+
 import 'package:ecommerceapp/screens/splash_page.dart';
 import 'package:flutter/material.dart';
 import 'Auth/login_page.dart';
